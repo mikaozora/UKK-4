907023fcc159e2eecc6f1cd7e2702b5f08411445
mikaozora <mikaozora08@gmail.com>
2021-10-12 22:51:42 +0700

    crud barang,level,masyarakat

--- Backend/index.js
+++ Backend/index.js
@@ -13,19 +13,19 @@ app.get("/", (req, res) => {
 })
 
 const history_lelang = require("./router/history_lelang")
-app.use("/history_lelang", history_lelang)
+app.use("/api/v1/history_lelang", history_lelang)
 
 const tb_barang = require("./router/tb_barang")
-app.use("/tb_barang", tb_barang)
+app.use("/api/v1/tb_barang", tb_barang)
 
 const tb_lelang = require("./router/tb_lelang")
-app.use("/tb_lelang", tb_lelang)
+app.use("/api/v1/tb_lelang", tb_lelang)
 
 const tb_level = require("./router/tb_level")
-app.use("/tb_level", tb_level)
+app.use("/api/v1/tb_level", tb_level)
 
 const tb_masyarakat = require("./router/tb_masyarakat")
-app.use("/tb_masyarakat", tb_masyarakat)
+app.use("/api/v1/tb_masyarakat", tb_masyarakat)
 
 const tb_petugas = require("./router/tb_petugas")
-app.use("/tb_petugas", tb_petugas)
+app.use("/api/v1/tb_petugas", tb_petugas)
