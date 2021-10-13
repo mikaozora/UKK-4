a12e59e8d4a4272dd0c8459224d656da8736c903
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2021-10-13 09:04:53 +0700

    Update tb_petugas.js

--- Backend/router/tb_petugas.js
+++ Backend/router/tb_petugas.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const multer = require("multer")
+const cors = require("cors")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -29,7 +31,7 @@ app.get("/", async (req, res) => {
 
 app.get("/:id", async (req, res) => {
     const param = {
-        id_petugas: req.param.id
+        id_petugas: req.params.id
     }
     await tb_petugas.findOne({ where: param })
         .then(result => {
