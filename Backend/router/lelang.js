5d11e63a8c8a67c49c472054ad6129add352df24
saputra1 <realary27@gmail.com>
2021-10-30 10:27:01 +0700

    proses lelang

--- Backend/router/lelang.js
+++ Backend/router/lelang.js
@@ -1,16 +1,93 @@
 const express = require("express")
 const multer = require("multer")
 // const { ENUM } = require("sequelize/types")
+const {LelangStatus} = require("./lelang.enum")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+const CronJob = require('cron').CronJob;
 const lelang = require("../models/index").lelang
 const history_lelang = require("../models/index").history_lelang
-const auth = require("../auth")
+const status = require('http-status');
+
+const checkLastPrice = async(time,timestamp,id) => {
+    const job = new CronJob(time, async () => {
+            const data = await history_lelang.findAll({where:{id:id}})
+            const {harga_akhir} = await lelang.findOne({where:{id:id}}).dataValues
+            const now = new Date().getTime()
+            let max = harga_akhir,id_masyarakat = null,before = harga_akhir
+            
+            if(now >= timestamp){
+            await lelang.update({harga_akhir:max,id_masyarakat:id_masyarakat,status:LelangStatus.DITUTUP},{where:{id:id}})
+            job.stop()
+            }
+
+            if(data){
+                data.forEach(e=>{
+                    if(e.price > max){
+                        max = e.penawaran_harga
+                        id_masyarakat = e.user_id
+                    }
+                })
+                if(max !== before){
+                    await lelang.update({harga_akhir:max,id_masyarakat:id_masyarakat},{where:{id:id}})
+                }
+            }
+        
+    });      
+    job.start()
+}
+
+app.put("/:id/start",async(req,res)=>{
+    const resultLelang = await lelang.findOne({id:req.params.id}),temp = resultLelang.dataValues
+    const now = new Date(),hours = now.setHours(now.getHours() + 1)
+    temp.status = LelangStatus.DIBUKA
+    temp.tgl_lelang = now
+    await lelang.update(temp,{where:{id:temp.id}})
+    await checkLastPrice('*/30 * * * * *',expired,temp.id) // cek and update lelang
+})
+
+app.post("/bid", async (req, res) => {
+    let current = new Date().toISOString().split('T')[0]
+    const {id_lelang,id_masyarakat,penawaran_harga} = req.body,{harga_akhir,status} = await lelang.findOne({where:{id:id_lelang}}).dataValues
+    const data = {
+        id_lelang:id_lelang,
+        id_masyarakat:id_masyarakat,
+        penawaran_harga:penawaran_harga
+    }
+
+    if(status === LelangStatus.DITUTUP){
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            error:"sorry,lelang belum dibuka atau sudah selesai",
+            status: HttpStatus.BAD_REQUEST,
+        })
+    }
+
+    if(harga_akhir > penawaran_harga){
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            error:
+            "sorry,penawaran harga kurang dari harga akhir!",
+            status: HttpStatus.BAD_REQUEST,
+        })
+    }
+
+    history_lelang.create(data)
+    .then(result => {
+        res.json({
+            message: "Data berhasil ditambahkan",
+            data: result
+        })
+    })
+        .catch(error => {
+            res.json({
+                message: error.message
+            })
+        })
+
+})
 
 
-app.get("/", auth, async (req, res) => {
+app.get("/", async (req, res) => {
     await lelang.findAll()
         .then(result => {
             res.json({
@@ -23,6 +100,7 @@ app.get("/", auth, async (req, res) => {
             })
         })
 })
+
 app.get("/:id", async (req, res) => {
     const param = {
         id_lelang: req.params.id
@@ -38,6 +116,7 @@ app.get("/:id", async (req, res) => {
             })
         })
 })
+
 app.post("/", async (req, res) => {
     let current = new Date().toISOString().split('T')[0]
     let data = {
@@ -46,7 +125,7 @@ app.post("/", async (req, res) => {
         harga_akhir: req.body.harga_akhir,
         id_masyarakat: req.body.id_masyarakat,
         id_petugas: req.body.id_petugas,
-        status: req.body.status 
+        status: LelangStatus.DITUTUP
     }
     lelang.create(data)
     .then(result => {
